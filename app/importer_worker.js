57dd5960d3db1819f237d7d5730f98a7da71a90b
Frank Zhao <frank_zsy@tongji.edu.cn>
2021-07-19 10:28:17 +0800

    refactor: update read and write stream to avoid memory consume
    Signed-off-by: Frank Zhao <frank_zsy@tongji.edu.cn>

--- app/importer_worker.js
+++ app/importer_worker.js
@@ -6,38 +6,6 @@ const { createGunzip } = require('zlib');
 const { createInterface } = require('readline');
 const ClickHouse = require('@apla/clickhouse');
 
-async function readRecords(filePath) {
-  return new Promise(async resolve => {
-    const ret = [];
-    try {
-      if (!existsSync(filePath)) {
-        return ret;
-      }
-      const unzip = createGunzip();
-      const fileStream = createReadStream(filePath).pipe(unzip).on('error', resolve);
-
-      const rl = createInterface({
-        input: fileStream,
-        crlfDelay: Infinity,
-      });
-
-      for await (const line of rl) {
-        try {
-          const item = JSON.parse(line);
-          ret.push(ParseFuncMap.get(item.type)?.call(undefined, item));
-        } catch {
-          console.log(`Error on parse record, line=${line}`);
-        }
-      }
-
-      resolve(ret);
-    } catch (e) {
-      console.error(e);
-      resolve([]);
-    }
-  });
-}
-
 async function readExistIds(f, dbConfig) {
   return new Promise(async resolve => {
     const ids = new Set();
@@ -61,40 +29,61 @@ async function readExistIds(f, dbConfig) {
   });
 }
 
-async function insertRecords(f, r, ids, dbConfig) {
+async function insertRecords(filePath, ids, dbConfig) {
   return new Promise(resolve => {
-    if (r.length <= 0) {
-      return resolve(true);
-    }
-    const client = new ClickHouse(dbConfig.serverConfig);
-    const stream = client.query(`INSERT INTO ${dbConfig.db}.${dbConfig.table}`, { format: 'JSONEachRow' }, e => {
-      if (e) {
-        console.error(f, e.message);
-        return resolve(false);
+    try {
+      if (!existsSync(filePath)) {
+        resolve(true);
+        return;
       }
-      resolve(true);
-    });
-    stream.on('error', e => {
-      console.error(f, e.message);
+
+      // open file stream
+      const unzip = createGunzip();
+      const fileStream = createReadStream(filePath).pipe(unzip).on('error', resolve);
+
+      const rl = createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+      });
+
+      // open database stream
+      const client = new ClickHouse(dbConfig.serverConfig);
+      const stream = client.query(`INSERT INTO ${dbConfig.db}.${dbConfig.table}`, { format: 'JSONEachRow' }, e => {
+        if (e) {
+          console.error(filePath, e.message);
+          return resolve(false);
+        }
+        resolve(true);
+      });
+      stream.on('error', e => {
+        console.error(filePath, e.message);
+        resolve(false);
+      });
+
+      // write file content into database
+      for await (const line of rl) {
+        try {
+          const item = JSON.parse(line);
+          const row = ParseFuncMap.get(item.type)?.call(undefined, item);
+          if (!ids.has(row.id)) {
+            stream.write(row);
+          }
+        } catch {
+          console.log(`Error on parse record, line=${line}`);
+        }
+      }
+
+      stream.end();
+
+    } catch (e) {
+      console.error(e);
       resolve(false);
-    });
-    for (const row of r) {
-      if (row && !ids.has(row.id)) {
-        ids.add(row.id);
-        stream.write(row);
-      }
     }
-    stream.end();
   });
 }
 
 parentPort?.on('message', async param => {
-  const [
-    records, ids,
-  ] = await Promise.all([
-    readRecords(param.filePath),
-    readExistIds(param.filePath, param.dbConfig),
-  ]);
-  const result = await insertRecords(param.filePath, records, ids, param.dbConfig);
+  const ids = await readExistIds(param.filePath, param.dbConfig);
+  const result = await insertRecords(param.filePath, ids, param.dbConfig);
   parentPort?.postMessage(result);
 });
